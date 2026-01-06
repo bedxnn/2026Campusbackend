036cb5dc7825daa11740a6e9158a0ffa816a6433
Your Name <148898409+bedxnn@users.noreply.github.com>
2026-01-06 17:45:15 -0500

    spring secuorty

--- src/main/java/campusbackend/auth/JwtService.java
+++ src/main/java/campusbackend/auth/JwtService.java
@@ -1,7 +1,14 @@
 package campusbackend.auth;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.util.Date;
 
 @Service
 public class JwtService {
@@ -9,4 +16,42 @@ public class JwtService {
     private String secret;
 
     private static final long EXPIRATION = 1000 * 60 * 60;
+
+    private Key key;
+
+    @PostConstruct
+    public void init() {
+        this.key = Keys.hmacShaKeyFor(secret.getBytes());
+    }
+
+    public String generateToken(String email) {
+        return Jwts.builder()
+                .setSubject(email)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION))
+                .signWith(key)
+                .compact();
+
+    }
+
+    public String extractEmail (String token) {
+        Claims claims = Jwts.parserBuilder()
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+
+        return claims.getSubject();
+    }
+
+    public boolean isTokenValid(String token) {
+        try {
+            extractEmail(token);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+
 }
